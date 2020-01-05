3562a1093016c1983a8bff25274fabc711b4e60c
gvoze32 <gvoze32@gmail.com>
2020-01-05 16:21:49 +0700

    first v1

---
+++ README.md
@@ -0,0 +1,10 @@
+A userscript that unblur Scribd document pages for unpaid visitors.
+
+### Getting started
+
+First you need to install either one of these user-script add-ons for your browser:
+
+* [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) for Firefox
+* [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) for Chrome
+
+Then open [this](https://githubusercontent.com/gvoze32/scribdunblurer/blob/master/scribdunblurer.user.js) to open dialog and click Install.
---
+++ scribdunblurer.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name          Scribd Unblurer
+// @namespace     gvoze32/scribdunblurer
+// @version       1.0
+// @description   Scribd document pages unblurer
+// @author        gvoze32
+// @grant         none
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @match         http://*.scribd.com/document/*
+// @match         https://*.scribd.com/document/*
+// ==/UserScript==
+
+setInterval(function() {
+ $('.absimg').css('opacity', '1.0');
+ $('.autogen_class_views_read2_page_blur_promo').remove();
+ $('.between_page_ads').remove();
+ $('.outer_page only_ie6_border blurred_page').remove();
+ $('.page_blur_promo').remove();
+ $('.promo').remove();
+ $('.page-blur-promo-overlay').remove();
+ $('.page_missing_explanation_inner').remove();
+ $('.text_layer').css('color', '#000');
+ $('.text_layer').css('text-shadow', '0px 0px 0px #000');
+}, 1000);

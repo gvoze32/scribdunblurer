22dc1e52ac0de2771f6b4e7658b5c3c0bb2883b2
gvoze32 <gvoze32@gmail.com>
2020-01-06 20:29:45 +0700

    better code

--- scribdunblurer.user.js
+++ scribdunblurer.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
-// @name          Scribd Unblurer
-// @namespace     gvoze32/scribdunblurer
-// @version       1.0
-// @description   Scribd document pages unblurer
-// @author        gvoze32
-// @grant         none
-// @require       https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
-// @match         http://*.scribd.com/document/*
-// @match         https://*.scribd.com/document/*
+// @name         Scribd Unblurer
+// @namespace    gvoze32/scribdunblurer
+// @version      1.0
+// @description  Scribd document pages unblurer
+// @author       gvoze32
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @match        *://*.scribd.com/document/*
 // ==/UserScript==
 
 setInterval(function() {
